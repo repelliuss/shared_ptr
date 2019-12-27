9bc66bdba0e43cf623b5bf0c26a31451683d9b94
repelliuss <officialrepelliuss@gmail.com>
2019-12-27 17:42:41 +0300

    Implemented MemoryManager class.
    		This class holds a data place in memory and deletes when told.

---
+++ include/repelliuss/MemoryManager.hpp
@@ -0,0 +1,23 @@
+#ifndef _MEMORYMANAGER_H_
+#define _MEMORYMANAGER_H_
+
+#include "Tracker.hpp"
+
+namespace rps {
+
+	template<class T, class Deleter>
+	class MemoryManager : public Tracker {
+
+	public:
+		MemoryManager(T *arg_data, Deleter arg_deleter) :
+			data(arg_data), deleter(arg_deleter) {}
+
+		void destroy() override { deleter(data); }
+
+	private:
+		T *data;
+		Deleter deleter;
+	};
+}
+
+#endif // _MEMORYMANAGER_H_
